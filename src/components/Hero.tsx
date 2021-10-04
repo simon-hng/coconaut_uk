98a0eb18d71155474782ba28f27760b1a49835f9
Simon Huang <simon.huang@tum.de>
2021-10-05 00:37:29 +0200

    add Hero file

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      {/* TODO: DOSEN SCHWEBEN HOCH UND RUNTER,
+			Animation mit designer klarstellen */}
+    </>
+  );
+};
+
+export default Hero;
