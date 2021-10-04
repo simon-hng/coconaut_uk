3be497eac25813964fb07ea8aa48ed9869021b27
Simon Huang <simon.huang@tum.de>
2021-10-05 00:38:06 +0200

    add sections to main page

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Hero from "../components/Hero";
+import About from "../components/About";
+
+const Index = () => {
+	return (
+		<main>
+			<Hero/>
+			<About/>
+		</main>
+	);
+};
+
+export default Index
