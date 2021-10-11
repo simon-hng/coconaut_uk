dc653daab66d61715e403538b04da8108a29a2e0
Simon Huang <simon.huang@tum.de>
2021-10-11 22:34:31 +0200

    add background

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,10 +1,20 @@
 import * as React from "react";
+import styled from "styled-components";
+
 import { Headline } from "@components/global";
+import BackgroundImage from "@images/hero/Coconaut_Skyline@1694x-100.jpg";
+
+const Background = styled.section`
+  width: 100vh;
+  background-image: url(${BackgroundImage});
+`;
 
 const Events = (): JSX.Element => {
   return (
     <>
       <Headline main="Join Our Events" centered />
+      <Background>
+      </Background>
     </>
   );
 };
