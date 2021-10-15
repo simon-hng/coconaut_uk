fdc75848d147e9ec08dbc40707450d33dda43a2f
Simon Huang <simon.huang@tum.de>
2021-10-15 12:47:40 +0200

    add revealing animation on scroll

--- src/components/Global/SectionWrapper.tsx
+++ src/components/Global/SectionWrapper.tsx
@@ -1,11 +1,14 @@
 import styled from 'styled-components';
+import { useInView } from 'react-intersection-observer';
+import * as React from 'react';
+import { motion } from 'framer-motion';
 
-interface sectionWrapperProps {
+interface sectionWrapperStyleProps {
   cover?: boolean;
 }
 
-export const SectionWrapper = styled.div.attrs(
-  (props: sectionWrapperProps) => ({
+export const SectionWrapperStyle = styled(motion.div).attrs(
+  (props: sectionWrapperStyleProps) => ({
     cover: props.cover,
   })
 )`
@@ -14,3 +17,35 @@ export const SectionWrapper = styled.div.attrs(
 
   ${(props) => !props.cover && 'max-width: 1400px;'}
 `;
+
+const sectionWrapperVariants = {
+  shown: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+    },
+  },
+  hidden: {
+    opacity: 0,
+  },
+};
+
+interface sectionWrapperProps {
+  children: React.ReactNode;
+  cover?: boolean;
+  id?: string;
+}
+
+export const SectionWrapper = (props: sectionWrapperProps) => {
+  const { ref, inView } = useInView();
+  return (
+    <SectionWrapperStyle
+      ref={ref}
+      variants={sectionWrapperVariants}
+      animate={inView ? 'shown' : 'hidden'}
+      {...props}
+    >
+      {props.children}
+    </SectionWrapperStyle>
+  );
+};
