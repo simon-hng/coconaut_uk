e5b62b3b9c247dbfdd10e5df1184016852b9bd31
Simon Huang <simon.huang@tum.de>
2021-10-23 12:51:32 +0200

    add reveal animation on mobile sections

--- src/components/Global/SectionTwoCol.tsx
+++ src/components/Global/SectionTwoCol.tsx
@@ -1,6 +1,8 @@
 import { SectionWrapper } from './SectionWrapper';
 import * as React from 'react';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 const SectionTwoColStyle = styled.section`
   display: flex;
@@ -11,7 +13,21 @@ const SectionTwoColStyle = styled.section`
   }
 `;
 
-const Content = styled.div`
+const contentVariants = {
+  shown: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  hidden: {
+    opacity: 0,
+  },
+};
+
+const Content = styled(motion.div).attrs(() => ({
+  variants: contentVariants,
+}))`
   padding: 0 2rem;
 
   @media (min-width: 900px) {
@@ -28,17 +44,26 @@ const Left = styled(Content)`
 `;
 
 const Right = styled(Content)``;
+
 interface sectionTwoColProps {
   id?: string;
   left: React.ReactElement;
   right: React.ReactElement;
 }
 
-export const SectionTwoCol = (props: sectionTwoColProps) => (
-  <SectionWrapper id={props.id}>
-    <SectionTwoColStyle>
-      <Left>{props.left}</Left>
-      <Right>{props.right}</Right>
-    </SectionTwoColStyle>
-  </SectionWrapper>
-);
+export const SectionTwoCol = (props: sectionTwoColProps) => {
+  const [left, leftInView] = useInView();
+  const [right, rightInView] = useInView();
+  return (
+    <SectionWrapper id={props.id}>
+      <SectionTwoColStyle>
+        <Left ref={left} animate={leftInView ? 'shown' : 'hidden'}>
+          {props.left}
+        </Left>
+        <Right ref={right} animate={rightInView ? 'shown' : 'hidden'}>
+          {props.right}
+        </Right>
+      </SectionTwoColStyle>
+    </SectionWrapper>
+  );
+};
